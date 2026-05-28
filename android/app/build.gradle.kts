20039fe1d6f14452cd02973ee7ce3bbb81dcc18a
Pikachubolk <noabolk@schoolsquid.xyz>
2026-05-28 16:46:15 +0200

    chore: Bump Android app version code to 33 and version name to 1.0.33

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.serika.maps"
         minSdk = 29
         targetSdk = 35
-        versionCode = 32
-        versionName = "1.0.32"
+        versionCode = 33
+        versionName = "1.0.33"
     }
 
     signingConfigs {
